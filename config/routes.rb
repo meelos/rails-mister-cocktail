889fec5cc831bd597578a976be92fbc00e1613e5
Myles Kilgallon <github@myles.uk>
2019-08-02 15:16:40 +0800

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
   resources :cocktails, except:[:edit,:update, :destroy]
-  # get 'cocktails', to:'cocktails#index'
-  # get 'cocktails/show'
-  # get 'cocktails/new'
-  # get 'cocktails/create'
-  # get 'cocktails/edit'
-  # get 'cocktails/update'
-  # get 'cocktails/destroy'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
